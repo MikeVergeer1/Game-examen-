1fbb03cd55fc99f6243a5ed7df8c4bcbe8a6c048
Mike Vergeer <574776@edu.rocmn.nl>
2025-06-18 23:26:45 +0200

    Added lifes update ball values

---
+++ Assets/Scripts/LifeManager.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class LifeManager : MonoBehaviour
+{
+    public static LifeManager Instance;
+
+    public TextMeshProUGUI livesText;
+    public int startingLives = 3;
+    private int currentLives;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            currentLives = startingLives;
+            UpdateLivesUI();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetLives(int value)
+    {
+        currentLives = value;
+        UpdateLivesUI();
+    }
+
+    public void LoseLife()
+    {
+        currentLives--;
+        UpdateLivesUI();
+
+        if (currentLives <= 0)
+        {
+            SceneManager.LoadScene("Mian Menu");
+        }
+    }
+
+    public int GetCurrentLives()
+    {
+        return currentLives;
+    }
+
+    private void UpdateLivesUI()
+    {
+        if (livesText != null)
+        {
+            livesText.text = $"Balls: {currentLives}";
+        }
+    }
+}
